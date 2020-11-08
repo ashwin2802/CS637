f1dbad05e59be0556f39059791139a1f987b7510
Kshitij1K <kabeer@iitk.ac.in>
2020-11-08 23:46:13 +0530

    Working intersection class
    Have modelled 1 and 4 conflict zones, should be enough for now

--- src/intersection.cpp
+++ src/intersection.cpp
@@ -2,18 +2,41 @@
 
 void Intersection::calculate_paths_one() {
     std::vector<int> single = {1};
-    for (int i=1; i<=4; i++) {
-        for (int j=1; j<=4; j++) {
-            if (i==j) continue;
-            paths.insert({std::pair(i,j), single});
+    for (int i = 1; i <= 4; i++) {
+        for (int j = 1; j <= 4; j++) {
+            if (i == j)
+                continue;
+            paths.insert({std::pair<int, int>(i, j), single});
         }
     }
 }
 
-Intersection::Intersection(int num) : n(num){
-    switch(n) {
+void Intersection::calculate_paths_four() {
+    paths.insert({std::pair<int, int>(1, 2), std::vector<int>({3, 1})});
+    paths.insert({std::pair<int, int>(1, 3), std::vector<int>({4, 1, 2})});
+    paths.insert({std::pair<int, int>(1, 4), std::vector<int>({4})});
+
+    paths.insert({std::pair<int, int>(2, 1), std::vector<int>({2, 3})});
+    paths.insert({std::pair<int, int>(2, 3), std::vector<int>({2})});
+    paths.insert({std::pair<int, int>(2, 4), std::vector<int>({2, 3, 4})});
+
+    paths.insert({std::pair<int, int>(3, 1), std::vector<int>({3})});
+    paths.insert({std::pair<int, int>(3, 2), std::vector<int>({3, 4, 1})});
+    paths.insert({std::pair<int, int>(3, 4), std::vector<int>({3, 4})});
+
+    paths.insert({std::pair<int, int>(4, 1), std::vector<int>({1, 2, 3})});
+    paths.insert({std::pair<int, int>(4, 2), std::vector<int>({1})});
+    paths.insert({std::pair<int, int>(4, 3), std::vector<int>({1, 2})});
+}
+
+Intersection::Intersection(int num)
+    : n(num) {
+    switch (n) {
         case 1:
             calculate_paths_one();
+            break;
+        case 4:
+            calculate_paths_four();
             break;
     }
 }
--- tests/intersection_test.cpp
+++ tests/intersection_test.cpp
@@ -3,13 +3,17 @@
 
 void print(int i) {
     Intersection inter(i);
+    std::cout << std::left << std::setw(10) << "Source" << std::setw(15) << "Destination" << "Conflict Zones" << std::endl;
+
     for (auto it_:inter.paths) {
-            std::cout << std::setw(3) << "Source" << std::setw(3) << "Destination" << std::cout << "Conflict Zones" << std::endl;
+        std::cout << std::left << std::setw(10) << it_.first.first  << std::setw(15) << it_.first.second;
+        for (auto vit_:it_.second) std::cout << vit_ << ", ";
+        std::cout << std::endl;
     }
 }
 
 int main() {
     
-    print(1);    
+    print(4);    
     return 0;
 }
