e8ff1d9841cd15e57939c849b0f00de7d7630a9e
Kshitij1K <kabeer@iitk.ac.in>
2020-11-10 23:36:27 +0530

    Add Type-1 edges

--- src/tcg.cpp
+++ src/tcg.cpp
@@ -5,18 +5,30 @@ TCG::TCG(Traffic T, Intersection I) {
     int i = 1;
 
     for (auto it : T.traffic) {
-        bool first_cz = true;
+        bool isfirst_cz = true;
         double start = it.first.second;
 
+        int first_cz = I.paths[std::pair<int, int>(it.first.first, it.second)][0];
+
         for (auto czit : I.paths[std::pair<int, int>(it.first.first, it.second)]) {
+            if (first_cz != czit) {
+                Edge e(EdgeType::TYPE_1);
+                e.wait_time = 0.1;
+
+                std::pair<std::pair<int, int>, Edge> dest_v(std::pair<int, int>(i, czit), e);
+                std::vector<std::pair<std::pair<int, int>, Edge>> dest = {dest_v};
+                edges.insert({std::pair<int, int>(i, first_cz), dest});
+                first_cz = czit;
+            }
+
             Vertex v = {.slack = INFINITY,
                 .pass_time = 1,
                 .start_time = start,
-                .first_j = first_cz,
+                .first_j = isfirst_cz,
                 .first_i = first_vehicle[it.first.first - 1],
                 .state = VertexState::WHITE};
             vertices.insert({std::pair<int, int>(i, czit), v});
-            first_cz = false;
+            isfirst_cz = false;
         }
 
         first_vehicle[it.first.first - 1] = false;
