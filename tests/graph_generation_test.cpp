2a944ddcb2ee10a69aa0bd278078db64375757f2
Kshitij1K <kabeer@iitk.ac.in>
2020-11-11 01:18:46 +0530

    Testing the edges

--- tests/graph_generation_test.cpp
+++ tests/graph_generation_test.cpp
@@ -1,7 +1,7 @@
-#include <traffic.hpp>
 #include <intersection.hpp>
-#include <tcg.hpp>
 #include <iomanip>
+#include <tcg.hpp>
+#include <traffic.hpp>
 
 std::string print_enum(int t) {
     switch (t) {
@@ -19,31 +19,45 @@ std::string print_enum(int t) {
     }
 }
 
+std::string print_enum(EdgeType type) {
+    switch (type) {
+        case EdgeType::TYPE_1:
+            return "Type 1";
+
+        case EdgeType::TYPE_2:
+            return "Type 2";
+
+        case EdgeType::TYPE_3:
+            return "Type 3";
+    }
+}
+
 int main() {
-    Traffic T(0.1, 30);
-    Intersection I(4);
+    Traffic T(0.1, 15);
+    Intersection I(1);
 
-    // T.traffic.clear();
+    T.traffic.clear();
 
-    // T.traffic.insert({std::pair<int, int>(4, 8), 1});
-    // T.traffic.insert({std::pair<int, int>(3, 11), 1});
-    // T.traffic.insert({std::pair<int, int>(1, 14), 4});
+    T.traffic.insert({std::pair<int, int>(4, 10), 1});
+    // T.traffic.insert({std::pair<int, int>(4, 11), 2});
+    T.traffic.insert({std::pair<int, int>(3, 12), 2});
+    // T.traffic.insert({std::pair<int, int>(1, 13), 2});
     TCG G(T, I);
 
-    std::cout << std::left << std::setw(10) << "Source" << std::setw(15) << "Destination" << "Time" << std::endl;
+    std::cout << std::left << std::setw(10) << "Source" << std::setw(15) << "Destination"
+              << "Time" << std::endl;
     for (auto it_ : T.traffic) {
-    //     print_enum(it_.first.first);
-    //     std::cout << std::endl;
-    //     // for (auto sit_ : it_.second) {
-    //         std::cout << sit_.first << " ";
-    //         print_enum(sit_.second);
-    //         std::cout << std::endl;
-    //     // }
-    //     std::cout << std::endl << std::endl;
-        std::cout << std::left << std::setw(10) << print_enum(it_.first.first)  << std::setw(15) << print_enum(it_.second) << it_.first.second << std::endl;
+        //     print_enum(it_.first.first);
+        //     std::cout << std::endl;
+        //     // for (auto sit_ : it_.second) {
+        //         std::cout << sit_.first << " ";
+        //         print_enum(sit_.second);
+        //         std::cout << std::endl;
+        //     // }
+        //     std::cout << std::endl << std::endl;
+        std::cout << std::left << std::setw(10) << print_enum(it_.first.first) << std::setw(15) << print_enum(it_.second) << it_.first.second << std::endl;
         // std::cout << vit_ << ", ";
     }
-
 
     // for (auto it_:inter.paths) {
     //     std::cout << std::left << std::setw(10) << it_.first.first  << std::setw(15) << it_.first.second;
@@ -67,6 +81,13 @@ int main() {
         std::cout << std::endl << std::endl;
     }
 
+    for (auto it : G.edges) {
+        for (auto vit : it.second) {
+            std::cout << it.first.first << ", " << it.first.second << "   ";
+            std::cout << vit.first.first << ", " << vit.first.second << "   ";
+            std::cout << print_enum(vit.second.type) << std::endl;
+        }
+    }
 
     return 0;
 }
