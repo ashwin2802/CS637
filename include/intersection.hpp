ac2d723754bbb888f409ef1cc26bac833dc12dfa
Kshitij1K <kabeer@iitk.ac.in>
2020-11-14 12:24:39 +0530

    Add intersection with 16 conflict zones

--- include/intersection.hpp
+++ include/intersection.hpp
@@ -4,8 +4,27 @@
 
 class Intersection {
   private:
+
+    /*
+        1
+    */
     void calculate_paths_one();  // compute path map
+
+    /*
+        4   3
+        1   2
+    */
     void calculate_paths_four();  // compute path map
+
+    /*
+            15      14
+        16  7   6   5   13
+            8       4   
+        9   1   2   3   12
+            10      11
+    */
+    void calculate_paths_sixteen();  // compute path map
+
 
   public:
     Intersection(int num);
